61a1315252fc1e82e38999d6eeafcbfa36d64082
VincinChristmas <VincinChristmas@hotmail.com>
2024-05-19 00:28:56 +0800

    added a 7kyu kata today

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+//NUMBER 3!!!
+
+/*
+Coding in function alienLanguage, function accept 1 parameter:str. str is a sentence.
+
+We translate the sentence into an alien language according to the following rules:
+
+Each word in the sentence is separated by spaces. The last letter of each word in the sentence 
+turns to lowercase, and the other letters are capitalized. Looks very strange? Because this is the form of alien language ;-)
+
+for example:
+
+alienLanguage("My name is John") should return "My NAMe Is JOHn"
+alienLanguage("this is an example") should return "THIs Is An EXAMPLe"
+alienLanguage("Hello World") should return "HELLo WORLd"
+A small hint: The first conversion of the entire string will make the code easier
+*/
+
+//MY SOLUTION
+function alienLanguage(str){
+  //coding here...
+  let newStr = str.toUpperCase().split(" ")
+  for (let i = 0; i < newStr.length; i++) {
+    
+   // newStr[i] = newStr[i].replace(newStr[i][newStr[i].length - 1], newStr[i][newStr[i].length - 1].toLowerCase()) 
+  newStr[i] = newStr[i].slice(0, newStr[i].length - 1) +newStr[i][newStr[i].length-1].toLowerCase()
+}
+  return newStr.join(" ")
+ } 
+
 //NUMBER 2 !!!
 
 /*
