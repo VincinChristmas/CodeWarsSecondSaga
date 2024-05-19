52afaf2166816ef953d8603b15026fd3af48f6ec
VincinChristmas <VincinChristmas@hotmail.com>
2024-05-19 16:24:47 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+//NUMBER 4!!!
+
+/*
+Coding in function fiveLine, function accept 1 parameter:s. s is a string.
+
+Please return a string of 5 lines(newline symbol is \n). The first line has one s; Second line have two s; and so on..Fifth line have five s;
+
+Note1: The two sides of the parameter s may contain some whitespace, please clear them before using s.
+
+Note2: Using a string template can make your job easier.
+
+Example:
+
+fiveLine("  a") should return "a\naa\naaa\naaaa\naaaaa"
+a
+aa
+aaa
+aaaa
+aaaaa       <---The effect when you console.log it
+fiveLine("  xy ") 
+should return "xy\nxyxy\nxyxyxy\nxyxyxyxy\nxyxyxyxyxy"
+xy
+xyxy
+xyxyxy
+xyxyxyxy
+xyxyxyxyxy  <---The effect when you console.log it
+
+*/
+
+//MY SOLUTION
+function fiveLine(s){
+  //coding here...
+  return `${s.trim()}\n${s.trim()}${s.trim()}\n${s.trim()}${s.trim()}${s.trim()}\n${s.trim()}${s.trim()}${s.trim()}${s.trim()}\n${s.trim()}${s.trim()}${s.trim()}${s.trim()}${s.trim()}`
+  
+}
+
+
 //NUMBER 3!!!
 
 /*
