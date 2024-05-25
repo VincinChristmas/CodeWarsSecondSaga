6765c6f385f46a235419e36de1ffe789ac3dbac2
VincinChristmas <VincinChristmas@hotmail.com>
2024-05-25 14:01:29 +0800

    added a new kata

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+//NUMBER 10!!!
+
+/*
+Is the string uppercase?
+Task
+Create a method to see whether the string is ALL CAPS.
+
+Examples (input -> output)
+"c" -> False
+"C" -> True
+"hello I AM DONALD" -> False
+"HELLO I AM DONALD" -> True
+"ACSKLDFJSgSKLDFJSKLDFJ" -> False
+"ACSKLDFJSGSKLDFJSKLDFJ" -> True
+*/ 
+
+//MY SOLUTION
+String.prototype.isUpperCase = function() {
+  // your code here
+  return this.toUpperCase() === this.toString()
+}
+
 //NUMBER 9!!!
 
 /*
