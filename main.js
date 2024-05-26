74c781ab3dbb093031e92655c2454037a91c6725
VincinChristmas <VincinChristmas@hotmail.com>
2024-05-26 15:43:25 +0800

    first time solving a problem using a do..while loop

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
+//NUMBER 12!!!
+
+/*
+Complete function padIt, which accepts 2 parameters:
+
+str: a string representing the string to pad. We will pad it with "*" symbols on its left side and on its right side.
+n: a number indicating how many times to pad the string.
+Behaviour
+You need to write a loop statement within the function that loops n times. Each time through the loop it will 
+add one * to str, alternating on which side it is padded: the first time will add a * to the left side of str, 
+the second time will add a * to the right side, and so on.
+
+Finally, return the padded string.
+
+*/ 
+
+//MY SOLUTION
+function padIt(str,n){
+  //coding here
+  
+  let number = 0 
+  let sumer = 1
+  let finalStr = str
+  
+  do {
+    number+=sumer
+    sumer++
+    
+    if (sumer % 2 !== 0) {
+      finalStr = finalStr + '*'
+    }
+    else if (sumer % 2 === 0) {
+      
+      finalStr = '*' + finalStr
+    }
+  } while (sumer <= n)
+  
+  
+  return finalStr
+  
+  
+}
+
 //NUMBER 11!!!
 
 /*
