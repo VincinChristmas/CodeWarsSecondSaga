163d84a08f76dbda252a63bb40784694fee8a2b3
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-03 12:40:17 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+//NUMBER 22!!!
+
+/*
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+ You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+*/ 
+
+//MY SOLUTION
+function removeChar(str){
+  return str.slice(1, str.length-1)
+ };
+
+
 //NUMBER 21!!!
 
 /*
