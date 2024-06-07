8b9c709c11dae7163d433d4b363e3178b797723d
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-07 19:43:50 +0800

    did a problem out of Eloquet JavaScript and decided to count it as a kata

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+//NUMBER 27!!!
+
+/*
+Looping a triangle
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+It may be useful to know that you can find the length of a string by writing .length after it.
+
+let abc = "abc";
+console.log(abc.length);
+// → 3
+*/ 
+
+//MY SOLUTION
+let hashMe = ''
+
+for (let i = 0; i < 7; i++) {
+    hashMe = hashMe + '#'
+    console.log(hashMe)
+}
+
+
+
 //NUMBER 26!!!
 
 /*
