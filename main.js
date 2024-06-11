7428f185c5048a9eba3cb91ef70d5bf4aa851c84
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-12 06:56:56 +0800

    added a failed kata today for ANKI

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+//NUMBER 29!!!
+
+/*
+Create a function shuffleIt. The function accepts two or more parameters. The first 
+parameter arr is an array of numbers, followed by an arbitrary number of numeric arrays. 
+Each numeric array contains two numbers, which are indices for elements in arr (the numbers will always be within bounds). 
+For every such array, swap the elements. Try to use all your new skills: arrow functions, the spread operator, destructuring, 
+and rest parameters.
+
+Example:
+
+shuffleIt([1,2,3,4,5],[1,2]) should return [1,3,2,4,5]
+shuffleIt([1,2,3,4,5],[1,2],[3,4]) should return [1,3,2,5,4]
+shuffleIt([1,2,3,4,5],[1,2],[3,4],[2,3]) should return [1,3,5,2,4]
+*/ 
+
+//MY SOLUTION
+function shuffleIt(arr,...p){
+  for (let i = 0; i<p.length; i++) {
+    let a = p[i][0];
+    let b = p[i][1];
+    [arr[b], arr[a]] = [arr[a], arr[b]];
+  }
+  return arr;
+}
+
+
+
 //NUMBER 28!!!
 
 /*
