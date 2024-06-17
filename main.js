0d332c49800aaa456aac784587b25f037791657e
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-17 15:55:29 +0800

    did a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,48 @@
+//NUMBER 35!!!
+
+/*
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+
+*/ 
+
+//MY SOLUTION
+function basicOp(operation, value1, value2){
+  //Code
+  let arithmetic = 0
+  switch (operation) {
+      case (operation = "+"):
+      arithmetic = 0 + value1 + value2;
+      break;
+      
+      case (operation = "-"):
+      arithmetic = 0 + value1 - value2;
+      break;
+      
+      case (operation = "*"):
+      arithmetic = 0 + (value1 * value2);
+      break;
+      
+      case (operation = "/"):
+      arithmetic = 0 + (value1 / value2);
+      break;
+      
+      default:
+      arithmetic = 0;
+  }
+  return arithmetic
+}
+
+
+
 //NUMBER 34!!!
 
 /*
