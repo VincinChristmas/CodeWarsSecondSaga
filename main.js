8a423bada135dee92f4999509699d989daffa996
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-19 06:59:18 +0800

    solved a new mathematics kata today

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+//NUMBER 38!!!
+
+/*
+Introduction
+The first century spans from the year 1 up to and including the year 100, the second century - 
+from the year 101 up to and including the year 200, etc.
+
+Task
+Given a year, return the century it is in.
+
+Examples
+1705 --> 18
+1900 --> 19
+1601 --> 17
+2000 --> 20
+2742 --> 28
+
+*/ 
+
+//MY SOLUTION
+function century(year) {
+  if (year < 100) {
+    return 1
+  }
+   return (year <= 99) ? 1 : Math.floor((year - 1) / 100) + 1;
+
+ // let centuryNumber = Math.floor(year/100) + 1
+}
+
+
 //NUMBER 37!!!
 
 /*
