121b325e22b63cc40172f0a8b8620a8c57016357
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-21 06:32:52 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+//NUMBER 40!!!
+
+/*
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+
+Example:
+h = 0
+m = 1
+s = 1
+
+result = 61000
+Input constraints:
+
+0 <= h <= 23
+0 <= m <= 59
+0 <= s <= 59
+
+*/ 
+
+//MY SOLUTION
+function past(h, m, s){
+  return (h * 3.6e+6) + (m * 60000) + (s * 1000)
+}
+
+
 //NUMBER 39!!!
 
 /*
