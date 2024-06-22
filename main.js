edbc3ce8f8942804bc74e7db51d5924e651ad3b1
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-22 08:30:04 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+//NUMBER 41!!!
+
+/*
+A hero is on his way to the castle to complete his mission. However, he's been told that the
+ castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, 
+ our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given 
+ number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return true if yes, false otherwise :)
+*/ 
+
+//MY SOLUTION
+function hero(bullets, dragons){
+  //Get Coding!
+    if ((bullets/2) >= dragons) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+
 //NUMBER 40!!!
 
 /*
