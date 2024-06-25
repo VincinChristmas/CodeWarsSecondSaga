a0cc83096ef45172233de04d54eb902317b4775e
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-26 06:26:17 +0800

    added a new 7kyu kata today

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+//NUMBER 47!!!
+
+/*
+Usually when you buy something, you're asked whether your credit card number, 
+phone number or answer to your most secret question is still correct. However,
+\ since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+Examples (input --> output):
+"4556364607935616" --> "############5616"
+     "64607935616" -->      "#######5616"
+               "1" -->                "1"
+                "" -->                 ""
+
+// "What was the name of your first pet?"
+"Skippy" --> "##ippy"
+"Nananananananananananananananana Batman!" --> "####################################man!"
+*/ 
+
+//MY SOLUTION
+function maskify(cc) {
+  let secretCollection = []
+  let numberReveal = []
+  if (cc.length > 4) {
+for (let i = 0; i < cc.length-4; i++) {
+  secretCollection.push('#')
+}
+for (let i = cc.length - 1; i >= cc.length - 4; i--) {
+  numberReveal.push(cc[i])
+}  
+      let finalCollection = secretCollection.concat(numberReveal.reverse())
+       return finalCollection.join('')
+    }
+    
+   else {
+     return cc
+   }
+
+}
+
 //NUMBER 46!!!
 
 /*
