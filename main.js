68d1fe0f06df6de05de6ddb19cd7eb3189f6a74e
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-26 13:44:33 +0800

    added a new fundamental kata today

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+//NUMBER 48!!!
+
+/*
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; 
+and month 11 (November), is part of the fourth quarter.
+
+Constraint:
+
+1 <= month <= 12
+*/ 
+
+//MY SOLUTION
+const quarterOf = (month) => {
+  
+  switch(true) {
+      case month == 1 || month == 2 || month == 3:
+      return 1;
+      break;
+      
+      case month == 4 || month == 5 || month == 6:
+      return 2;
+      break;
+      
+      case month == 7 || month == 8 || month == 9:
+      return 3;
+      break;
+      
+      case month == 10 || month == 11 || month == 12:
+      return 4;
+      break;
+      
+      default:
+      return "please enter a month"
+  }
+  
+  
+}
+
+
 //NUMBER 47!!!
 
 /*
