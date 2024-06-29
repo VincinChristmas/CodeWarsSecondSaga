f1295df361fd8263390ba709d3cc638e245bb06c
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-29 21:48:36 +0800

    added a simple kata

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+//NUMBER 56!!!
+
+/*
+You are given two interior angles (in degrees) of a triangle.
+
+Write a function to return the 3rd.
+
+Note: only positive integers will be tested.
+*/
+
+//MY SOLUTION
+function otherAngle(a, b) {
+  let c = 180 - a - b
+  return c
+}
+
+
 //NUMBER 55!!!
 
 /*
