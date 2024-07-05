d87020180491cb4acac147080ad413d69f04e780
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-06 06:25:54 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,51 @@
+//NUMBER 67!!!
+
+/*
+In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, 
+for the sake of simplicity, are named with letters from a to m.
+
+The colors used by the printer are recorded in a control string. For example a "good" control string would 
+be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g.
+ aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+You have to write a function printer_error which given a string will return the error rate of the printer as a 
+string representing a rational whose numerator is the number of errors and the denominator the length of the control string. 
+Don't reduce this fraction to a simpler expression.
+
+The string has a length greater or equal to one and contains only letters from ato z.
+
+Examples:
+s="aaabbbbhaijjjm"
+printer_error(s) => "0/14"
+
+s="aaaxbbbbyyhwawiwjjjwwm"
+printer_error(s) => "8/22"
+
+*/
+
+//MY SOLUTION
+function printerError(s) {
+  //make an array of letters a - m
+ //iterate through the string
+ //if anny letter of the string is included in the array of letters, add 1 to sum
+ //return sum over the length of the string
+ //if no error , return 0 over the length of the string
+ let arr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
+ let newS = s.split('');
+ let sum = 0;
+ newS.forEach((letter) => {
+   if (arr.includes(letter)) {
+     // Do nothing
+   } else {
+     sum += 1;
+   }
+ });
+ return `${sum}/${newS.length}`;
+}
+
+
 //NUMBER 66!!!
 
 /*
