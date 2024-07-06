84c7020333a501163472db0292a742070f67a770
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-06 19:05:51 +0800

    added a new kata today. binary addition one

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+//NUMBER 69!!!
+
+/*
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+
+*/
+
+//MY SOLUTION
+const binaryArrayToNumber = arr => {
+  let xSum = -1
+  let arrSum = 0
+  for (let i = arr.length - 1; i >=0; i--) {
+    xSum = xSum + 1
+    arr[i] = arr[i] * (2**xSum)
+    arrSum = arrSum + arr[i]
+  }
+  return arrSum
+};
+
+
 //NUMBER 68!!!
 
 /*
