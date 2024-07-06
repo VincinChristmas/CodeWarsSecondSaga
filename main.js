c02bb6d795a821a7d86b0bce947f61a0869fbe67
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-06 18:48:39 +0800

    added a new kata

--- main.js
+++ main.js
@@ -1,3 +1,62 @@
+//NUMBER 68!!!
+
+/*
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement.
+
+*/
+
+//MY SOLUTION
+function switchItUp(number){
+  //Write your own Code!
+    switch(true) {
+        case number === 0:
+        return 'Zero'
+        break;
+        
+        case number === 1:
+        return 'One'
+        break;
+        
+        case number === 2:
+        return 'Two'
+        break;
+        
+        case number === 3:
+        return 'Three'
+        break;
+        
+        case number === 4:
+        return 'Four'
+        break;
+        
+        case number === 5:
+        return 'Five'
+        break;
+        
+        case number === 6:
+        return 'Six'
+        break;
+        
+        case number === 7:
+        return 'Seven'
+        break;
+        
+        case number === 8:
+        return 'Eight'
+        break;
+        
+        case number === 9:
+        return 'Nine'
+        break;
+    }
+  }
+
 //NUMBER 67!!!
 
 /*
