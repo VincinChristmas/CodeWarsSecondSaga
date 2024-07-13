35492586d86230b61076b34a2e3ef041a320298e
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-13 11:57:24 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,36 @@
+//NUMBER 80
+
+/*
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained (Read the notes for more detail about it)
+Example
+With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+
+1 * (2 + 3) = 5
+1 * 2 * 3 = 6
+1 + 2 * 3 = 7
+(1 + 2) * 3 = 9
+So the maximum value that you can obtain is 9.
+*/
+
+//MY SOLUTION
+function expressionMatter(a, b, c) {
+  
+  let first = a + b + c;  
+  let second = a * b * c; 
+  let third = (a + b) * c; 
+  let fourth = a * (b + c); 
+  
+  
+  let results = [first, second, third, fourth];
+
+  
+  let maxResult = Math.max(...results);
+
+  return maxResult;
+}
+
+
 //NUMBER 79
 
 /*Complete the function/method so that it returns the url with anything after the anchor (#) removed.
