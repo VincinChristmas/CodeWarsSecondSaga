32aebb3ded7cb878147d7155ce1329e94ca7f3ba
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-14 23:20:24 +0800

    added another kata

--- main.js
+++ main.js
@@ -1,3 +1,109 @@
+//NUMBER 85
+
+/*
+Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+The Task
+Think of a way to store the languages as a database. The languages are listed below so you can copy and paste!
+Write a 'welcome' function that takes a parameter 'language', with a type String, and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+The Database
+[ ("english", "Welcome")
+, ("czech", "Vitejte")
+, ("danish", "Velkomst")
+, ("dutch", "Welkom")
+, ("estonian", "Tere tulemast")
+, ("finnish", "Tervetuloa")
+, ("flemish", "Welgekomen")
+, ("french", "Bienvenue")
+, ("german", "Willkommen")
+, ("irish", "Failte")
+, ("italian", "Benvenuto")
+, ("latvian", "Gaidits")
+, ("lithuanian", "Laukiamas")
+, ("polish", "Witamy")
+, ("spanish", "Bienvenido")
+, ("swedish", "Valkommen")
+, ("welsh", "Croeso")
+]
+*/
+
+//MY SOLUTION
+function greet(language) {
+  
+  switch(true) {
+      case (language === "english"):
+      return "Welcome";
+      break;
+      
+      case (language === "czech"):
+      return "Vitejte";
+      break;
+      
+      case (language === "danish"):
+      return "Velkomst";
+      break;
+      
+      case (language === "dutch"):
+      return "Welkom";
+      break;
+      
+      case (language === "estonian"):
+      return "Tere tulemast";
+      break;
+      
+      case (language === "finnish"):
+      return "Tervetuloa";
+      break;
+      
+      case (language === "flemish"):
+      return "Welgekomen";
+      break;
+      
+      case (language === "french"):
+      return "Bienvenue";
+      break;
+      
+      case (language === "german"):
+      return "Willkommen";
+      break;
+      
+      case (language === "irish"):
+      return "Failte";
+      break;
+      
+      case (language === "italian"):
+      return "Benvenuto";
+      break;
+      
+      case (language === "latvian"):
+      return "Gaidits";
+      break;
+      
+      case (language === "lithuanian"):
+      return "Laukiamas";
+      break;
+      
+      case (language === "polish"):
+      return "Witamy";
+      break;
+      
+      case (language === "spanish"):
+      return "Bienvenido";
+      break;
+      
+      case (language === "swedish"):
+      return "Valkommen";
+      break;
+      
+      case (language === "welsh"):
+      return "Croeso";
+      break;
+      
+      default:
+      return "Welcome"
+  } 
+}
+
 //NUMBER 84
 
 /*
