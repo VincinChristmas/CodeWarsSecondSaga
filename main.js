70b094637834e9c27402f3ad2287ac734477c7fd
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-15 20:29:14 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+//NUMBER 86
+
+/*
+Coding in function isolateIt. function accept 1 parameters arr, it's a string array. Your task is to put 
+a character "|" into the middle of each element.
+
+If the string length is an even number, use the insert method. for example: "abcd" should became "ab|cd". 
+"|" should be inserted between ab and cd.
+
+If the string length is an odd number, use the replacement method. for example: "abcde" should became 
+"ab|de". Character c will be replaced by |.
+
+The original array should not be changed, you need to return a new array(if you use the map method, you 
+do not need to worry about this).
+
+Example
+isolateIt(["abcd","efgh"]) should return ["ab|cd","ef|gh"]
+isolateIt(["abcde","fghij"]) should return ["ab|de","fg|ij"]
+isolateIt(["1234","56789"]) should return ["12|34","56|
+
+*/
+
+//MY SOLUTION
+function isolateIt(arr) {
+  return arr.map(item => {
+      const midIndex = Math.floor(item.length / 2);
+      if (item.length % 2 === 0) {
+          
+          return item.slice(0, midIndex) + '|' + item.slice(midIndex);
+      } else {
+          
+          return item.slice(0, midIndex) + '|' + item.slice(midIndex + 1);
+      }
+  });
+}
+
+
 //NUMBER 85
 
 /*
