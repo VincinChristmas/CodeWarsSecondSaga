57234a8b5c9a1b97e5a0efa3ba4b4749f6e7337a
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-16 00:34:33 +0800

    added another kata

--- main.js
+++ main.js
@@ -1,3 +1,32 @@
+//NUMBER 89
+
+
+/*
+Coding in function bigToSmall. function accept 1 parameter arr(2D number array).
+
+Your task is: First to all, refer to the example above, flat output arr to a one-dimensional array.
+
+And then sort array in descending order.
+
+Finally, use the separator ">" to connect the elements into a string.
+
+Don't complain about the situation like 1>1 is not reasonable, it is just a separator.
+
+Some example:
+
+bigToSmall([[1,2],[3,4],[5,6]]) should return "6>5>4>3>2>1"
+bigToSmall([[1,3,5],[2,4,6]]) should return "6>5>4>3>2>1"
+bigToSmall([[1,1],[1],[1,1]]) should return "1>1>1>1>1"
+*/
+
+
+//MY SOLUTION
+function bigToSmall(arr){
+  //coding here...
+  let newArr = [].concat(...arr)
+  return newArr.sort((a,b)=>a-b).reverse().join('>')
+}
+
 //NUMBER 88
 
 
