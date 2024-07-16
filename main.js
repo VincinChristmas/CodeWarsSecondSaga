80f72a41cdc35a65f94a2ad6f3e2e1165645d111
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-16 23:21:17 +0800

    added another kata

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 //NUMBER 90
 
+/*
+Messi's Goal Total
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Task
+Create these three variables and store the appropriate values using the table above:
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+*/
+
+//MY SOLUTIONS
+var  laLigaGoals = 43
+var  championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+
+//NUMBER 90
 
 /* 
 Coding in function mirrorImage. function accept 1 parameter arr, it's a number array. 
