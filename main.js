ab54e13279f2ef494b3e6e5509cc1a55161f86b1
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-18 15:07:52 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+//NUMBER 92
+
+/*
+Coding in function blackAndWhite. function accept 1 parameter arr(a number array).
+
+If arr is not an array, function should return:
+
+"It's a fake array"
+If arr contains elements 5 and 13, function should return:
+
+"It's a black array"
+If arr contains neither 5 nor 13, function should return:
+
+"It's a white array"
+Examples
+blackAndWhite(5,13) should return "It's a fake array"
+blackAndWhite([5,13]) should return "It's a black array"
+blackAndWhite([5,12]) should return "It's a white array" 
+Using string template and ternary operator can make your work easier.
+*/
+
+//My SOLUTION
+function blackAndWhite(arr){
+  //coding here...
+  if (Array.isArray(arr) === false) {
+    return "It's a fake array"
+  }
+  
+  else if ((arr.includes(5)===true) && (arr.includes(13)===true)) {
+    return "It's a black array"
+  }
+  else if ((arr.includes(5)=== false) || (arr.includes(13)===false)) {
+    return "It's a white array"
+  }
+  
+}
+
+
 //NUMBER 91
 
 
