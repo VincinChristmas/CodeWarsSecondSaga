897b10f1f0e6191aa8b52127e1f5522d50690b56
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-20 10:23:36 +0800

    added a new kata today

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
+//NUMBER 96
+
+/*
+
+*/
+
+//MY NUMBER
+
+//NUMBER 95
+
+/*
+Coding in function maxMin. function accept 2 parameter arr1 and arr2. They are two number array and have the same number of elements.
+
+First, calculate the difference of the same index of the arr1 and arr2. Like this:
+
+[1,3,5]
+ | | |   --->  8, 5, 2
+[9,8,7]
+Please note that the difference is positive. Such as the above 1 and 9, the difference should be 8, not -8. 
+I think abs() can help you get the correct result ;-)
+
+Then find the maximum and minimum values of them, and return the results in the form of an array. Like this:
+
+  maxvalue , minvalue
+[    8     ,    2     ]
+Examples
+maxMin([1,3,5],[9,8,7])               should return [8,2]
+maxMin([1,10,100,1000],[0,0,0,0])     should return [1000,1]
+maxMin([10,20,30,40],[111,11,1,-111]) should return [151,9]
+*/
+
+//MY SOULUTION
+function maxMin(arr1,arr2){
+  //coding here...
+  let emptyArr = []
+  let newArr = []
+  for (let i = 0; i < arr2.length; i++) {
+    emptyArr.push(Math.abs(arr2[i]-arr1[i]))
+  }
+  newArr.push(Math.max(...emptyArr)) 
+    newArr.push(Math.min(...emptyArr))
+  return newArr
+}
 
 //NUMBER 94
 
