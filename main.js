e695645bdffa7066d81d6468bed4d04dde179b6c
VincinChristmas <VincinChristmas@hotmail.com>
2024-07-20 11:30:13 +0800

    trained on another kata

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+//NUMBER 97
+
+/*
+Coding in function rndCode. Your task is to generate a random verification code. In accordance with the following rules:
+
+the code length should be 8;
+
+The 1st and 2nd characters should be two uppercase letters. The range is "ABCDEFGHIJKLM".
+
+The 3rd-6th characters should be four numbers.
+
+the 7th and 8th characters should be two symbols. The range is "~!@#$%^&*".
+
+If Your code runs 100 times, It should generate 100 non duplicate verification codes.
+
+Some valid verification code examples:
+*/
+
+//MY SOLUTION
+function rndCode(){
+  var randomRange = function (range, count) {
+    var sequence = [];
+    for (var i=0; i<count; i++) {
+      sequence.push(range[~~(range.length*Math.random())]);
+    }
+    
+    return sequence.join("");
+  }
+  
+  return randomRange('ABCDEFGHIJKLM', 2) + randomRange('0123456789', 4) + randomRange('~!@#$%^&*', 2);
+}
+
 //NUMBER 96
 
 /*
